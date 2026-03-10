706ee8c040e5816e42d22b5b97f2f2b5dd3182f2
Jose Robinson <dev@joserobinson.com>
2026-03-10 00:44:44 -0400

    chore: configure jest

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,30 @@
 import Fastify from 'fastify'
 
-const fastify = Fastify({ logger: true })
+export const app = Fastify({ logger: true })
 
-fastify.get('/', async function handler (request, reply) {
+app.get('/', async function handler (request, reply) {
   return 'hello 👋🏻'
 })
 
-fastify.listen({ port: 3000 })
+app.post('/telegram-webhook', async (request, reply) => {
+  try {
+    const update: unknown = request.body
+
+    // await handleUpdate(update)
+
+    // res.status(200).json({ ok: true })
+  } catch (error) {
+    // logger.error('Error processing request:', error)
+    // res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen({ port: 3000 }, (err) => {
+    if (err) {
+      app.log.error(err)
+      process.exit(1)
+    }
+  })
+}
+
