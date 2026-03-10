cb90667f896e255a4524ef67c46e74794f3ba324
Jose Robinson <dev@joserobinson.com>
2026-03-10 10:55:09 -0400

    chore: configure ESLint, Prettier, EditorConfig, and other minor tweaks

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+);
---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'all',
+  printWidth: 80,
+  endOfLine: 'lf',
+  semi: true,
+  singleQuote: true,
+};
+
+module.exports = config;
